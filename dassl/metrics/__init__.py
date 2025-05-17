43fa3fa9ff2692882871af981fce6801768c4a20
vijaykanak <161119449+vijaykanak@users.noreply.github.com>
2025-05-17 19:22:22 +0530

    Update __init__.py

--- dassl/metrics/__init__.py
+++ dassl/metrics/__init__.py
@@ -1,4 +1,4 @@
-from .accuracy import compute_accuracy
+from .accuracy import compute_accuracy, compute_accuracy_i2t, compute_accuracy_t2i
 from .distance import (
     cosine_distance, compute_distance_matrix, euclidean_squared_distance
 )
